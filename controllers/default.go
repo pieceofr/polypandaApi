e8b98bb148879f23eb9c0d5c3e7bda1c304f6e87
pieceofr <pieceofr@gmail.com>
2018-04-13 21:16:37 +0800

    Return git commit hex code in Status Value field

--- controllers/default.go
+++ controllers/default.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"bytes"
+	"os/exec"
 	"polypanda/polypandaApi/models"
 
 	"github.com/astaxie/beego"
@@ -21,7 +23,14 @@ func (c *MainController) Get() {
 /*GetStatus keep alive function*/
 func (c *MainController) GetStatus() {
 	var ret models.RetSimple
+	out, err := exec.Command("git", "rev-parse", "--short", "HEAD").Output()
+	//out, err := exec.Command("cat README.md").Output()
+	if err != nil {
+		ret.Message = err.Error()
+		ret.Value = ""
+	}
 	ret.StatusOK()
+	ret.Value = string(bytes.TrimRight(out, "\x0a"))
 	c.Data["json"] = &ret
 	c.ServeJSON()
 
